35c9cecab047a6360dd300d7c76aec2ab2543421
polosa4 <polosa4@gmail.com>
2020-08-08 17:54:18 -0500

    git init

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import ReactNavbar from "react-responsive-animate-navbar";
+ 
+class Navbar extends Component {
+  render() {
+    return (
+      <ReactNavbar
+        color="rgb(25, 25, 25)"
+        username="Oleg"
+        logo="https://svgshare.com/i/KHh.svg"
+        menu={[
+          { name: "HOME", to: "/" },
+          { name: "ARTICLES", to: "/articles" },
+          { name: "ABOUT ME", to: "/about" },
+          { name: "CONTACT", to: "/contact" },
+        ]}
+        social={[
+          {
+            name: "Linkedin",
+            url: "https://www.linkedin.com/in/nazeh-taha/",
+            icon: ["fab", "linkedin-in"],
+          },
+          {
+            name: "Facebook",
+            url: "https://www.facebook.com/nazeh200/",
+            icon: ["fab", "facebook-f"],
+          },
+          {
+            name: "Instagram",
+            url: "https://www.instagram.com/nazeh_taha/",
+            icon: ["fab", "instagram"],
+          },
+          {
+            name: "Twitter",
+            url: "http://nazehtaha.herokuapp.com/",
+            icon: ["fab", "twitter"],
+          },
+        ]}
+      />
+    );
+  }
+}
+
+export default Navbar;
